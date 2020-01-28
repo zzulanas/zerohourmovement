b401e6774842c7ba6ce431fd8d2b5a8f554b53e8
Zachary Zulanas <zzulanas@gmail.com>
2020-01-28 11:33:07 -0800

    changed styles a bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,40 +14,51 @@ import {
 import "../css/bg.css"
 
 const boxstyle = {
-  left:'35%',
-  padding:'10vmin',
-  position:'fixed',
-  textAlign:'center',
-  top:'50%',
-  transform:'translate(-50%, -50%)'
+  left: '35%',
+  padding: '10vmin',
+  position: 'fixed',
+  textAlign: 'center',
+  top: '50%',
+  transform: 'translate(-50%, -50%)'
 }
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home"/>
+    <SEO title="Home" />
     <div class="bg"></div>
     <div class="bg bg2"></div>
     <div class="bg bg3"></div>
     <Box
-        style={boxstyle}
-        sx={{
-          display: 'grid',
-          gridGap: 3,
-          backgroundColor: 'transparent'      // theme.space[3]
-        }}
+      style={boxstyle}
+      sx={{
+        backgroundColor: 'transparent'      // theme.space[3]
+      }}
     >
       <Card>
         <Text
-        fontFamily="Norwester"
-        fontSize={[4,5,6]}
-        m={15}>Zero Hour Movement</Text>
-        <Link href="https://www.instagram.com/zerohourmvmt/"><Button className="pulse" variant='outline' mr={2} fontFamily="Norwester" backgroundColor='secondary'>Instagram</Button></Link>
-        <Link href="https://twitter.com/zerohourmvmt/"><Button className="pulse" variant='outline' mr={2} fontFamily="Norwester" backgroundColor='secondary'>Twitter</Button></Link>
-        <Link href="https://google.com"  sx={{
-          ':hover':{
-            cursor: 'pointer'
-          }
-        }}><Button className="pulse" variant='outline' mr={2} fontFamily="Norwester" backgroundColor='secondary'>Donate</Button></Link>
+          fontFamily="Norwester"
+          fontSize={6}
+          display="inline"
+          m={15}>Zero Hour Movement</Text>
+        <Box><Link href="https://google.com"><Button className="pulse" variant='outline' sx={{
+          '&:hover': {
+            backgroundColor: 'blue',
+          },
+          cursor: 'pointer'
+        }} m={2} p={3} fontSize={4} fontFamily="Norwester" backgroundColor='secondary'>Donate</Button></Link>
+          <Link href="https://www.instagram.com/zerohourmvmt/"><Button className="pulse" variant='outline' m={2} fontFamily="Norwester" backgroundColor='secondary' sx={{
+          '&:hover': {
+            backgroundColor: 'blue',
+          },
+          cursor: 'pointer'
+        }}>Instagram</Button></Link>
+          <Link href="https://twitter.com/zerohourmvmt/"><Button className="pulse" variant='outline' m={2} sx={{
+          '&:hover': {
+            backgroundColor: 'blue',
+          },
+          cursor: 'pointer'
+        }} fontFamily="Norwester" backgroundColor='secondary'>Twitter</Button></Link>
+        </Box>
       </Card>
     </Box>
   </Layout>
