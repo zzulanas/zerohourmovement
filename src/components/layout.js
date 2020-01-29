040ab8c439031f31d521e10481922b9ef582c3e3
Zachary Zulanas <zzulanas@gmail.com>
2020-01-28 20:53:48 -0800

    fixed zerohour link

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,7 +41,7 @@ const Layout = ({ children }) => {
             color='white'
             bg='transparent'
             alignItems='center'>
-            <Link href="thisiszerohour.org"><img src={logo} style={{maxWidth:80, margin:10}}/></Link>
+            <Link href="http://thisiszerohour.org"><img src={logo} style={{maxWidth:80, margin:10}}/></Link>
             <Box mx='auto' />
           </Flex>
           <main>{children}</main>
